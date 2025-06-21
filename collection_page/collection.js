79741a3f01fbc69e3985bbb68b5b3774882c545d
hyuyu1012 <hanhyeongu495@gmail.com>
2025-06-21 13:03:04 +0900

    기본 collections ui

--- collection_page/collection.js
+++ collection_page/collection.js
@@ -6,24 +6,3 @@ collections.forEach((collection, index) => {
   })
 })
 
-
-/*
-collections[0].addEventListener('click' , function() {
-  alert(0);
-})
-collections[1].addEventListener('click' , function() {
-  alert(1);
-})
-collections[2].addEventListener('click' , function() {
-  alert(2);
-})
-collections[3].addEventListener('click' , function() {
-  alert(3);
-})
-collections[4].addEventListener('click' , function() {
-  alert(4);
-})
-collections[5].addEventListener('click' , function() {
-  alert(5);
-})
-  */
